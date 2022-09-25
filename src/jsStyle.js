d095de7d87c8c93c7755cfe1de277643b3c2d6f0
sarajorheim <110483192+sarajorheim@users.noreply.github.com>
2022-09-25 13:38:58 +0200

    added link ti F and C and also changed css

--- src/jsStyle.js
+++ src/jsStyle.js
@@ -87,12 +87,16 @@ function getCurrentPosition(event) {
 }
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 function displayCelsiusTemperature(event) {
   event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
