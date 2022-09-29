16fa426e7cfaca077a247f85430d0700c3938c3d
sarajorheim <110483192+sarajorheim@users.noreply.github.com>
2022-09-29 11:52:17 +0200

    stored api respons in variable-forecast.

--- src/jsStyle.js
+++ src/jsStyle.js
@@ -41,31 +41,46 @@ let month = months[now.getMonth()];
 
 dateLi.innerHTML = `${day}, ${month} ${currentDate}, ${currentHour}: ${currentMinutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForcast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
           <div class="col-2">
-            <div class="weather-forcast-date">${day}</div>
+            <div class="weather-forcast-date">${formatDay(forecastDay.dt)}</div>
             <img
-              src="https://openweathermap.org/img/wn/50d@2x.png"
+              src="https://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
               alt=""
               width="42"
             />
             <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperatures-max">18°</span>
-              <span class="weather-forecast-temperatures-min">12°</span>
+              <span class="weather-forecast-temperatures-max">${Math.round(
+                forecastDay.temp.max
+              )}°</span>
+              <span class="weather-forecast-temperatures-min">${Math.round(
+                forecastDay.temp.min
+              )}°</span>
             </div>
           </div>
        
   `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
@@ -154,3 +169,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("New York");
+
+//lesson 6 done
